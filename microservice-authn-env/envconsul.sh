ec0652071ddfaffa268309c7e518406ba936848a
javedmshah <javedmshah@users.noreply.github.com>
2018-04-28 16:19:25 -0700

    Create envconsul.sh

---
+++ microservice-authn-env/envconsul.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export SERVICE_HOME=/opt/authn-microservice
+export CLASSPATH="$SERVICE_HOME/lib/*:$SERVICE_HOME/framework/*:$SERVICE_HOME/bin/*"
+
+__PROJECT_HOME="$SERVICE_HOME"
+if [ -n "$PROJECT_HOME" ]; then
+    __PROJECT_HOME="$PROJECT_HOME"
+fi
+
+echo "SERVICE_HOME is $SERVICE_HOME"
+echo "PROJECT_HOME is $__PROJECT_HOME"
+
+__JAVA_OPTS="-server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm"
+if [ -n "$JAVA_OPTS" ]; then
+    __JAVA_OPTS="$JAVA_OPTS"
+fi
+
+# start envconsul with address for consul and use key namespace for ms-authn to spawn new container
+/usr/bin/envconsul -log-level debug -consul=192.168.99.100:31100 -pristine -prefix=ms-authn /bin/sh -c bin/docker-entrypoint.sh 
